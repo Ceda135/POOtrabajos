f184cef76b55a1af1a74da05afa387f8b51ab6fc
Ceda135 <cdherrera2005@gmail.com>
2025-04-25 21:57:12 -0500

    Cambio2

---
+++ src/tallergitcesar/Cambio2.java
@@ -0,0 +1,13 @@
+package tallergitcesar;
+
+public class Cambio2 {
+    private String materia;
+
+    public Cambio2(String materia) {
+        this.materia = materia;
+    }
+
+    public void enseñar() {
+        System.out.println("Estoy enseñando la materia: " + materia);
+    }
+}
